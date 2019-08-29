9c762faf03a1e622fb0092fc8504132556425aaf
Jack Danger <github@jackcanty.com>
2019-08-29 15:43:58 -0700

    This require is helpful

--- lib/phonetics/levenshtein.rb
+++ lib/phonetics/levenshtein.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
+require_relative '../phonetics'
 require_relative 'c_levenshtein'
+
 # Using the Damerau version of the Levenshtein algorithm, with phonetic feature
 # count used instead of a binary edit distance calculation
 #
-# This implementation is almost entirely taken from the damerau-levenshtein gem
+# This implementation was dually inspired by the damerau-levenshtein gem
 # (https://github.com/GlobalNamesArchitecture/damerau-levenshtein/tree/master/ext/damerau_levenshtein).
-# The implementation is modified based on "Using Phonologically Weighted
-# Levenshtein Distances for the Prediction of Microscopic Intelligibility" by
-# Lionel Fontan, Isabelle Ferrané, Jérôme Farinas, Julien Pinquier, Xavier
-# Aumont, 2016
+# and "Using Phonologically Weighted Levenshtein Distances for the Prediction
+# of Microscopic Intelligibility" by Lionel Fontan, Isabelle Ferrané, Jérôme
+# Farinas, Julien Pinquier, Xavier Aumont, 2016
 # https://hal.archives-ouvertes.fr/hal-01474904/document
 module Phonetics
   module Levenshtein
